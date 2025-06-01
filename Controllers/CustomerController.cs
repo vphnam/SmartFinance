d6c32fae042c2145f0f0f44cff163c6e2174530c
namvph <hoainam11134@gmail.com>
2025-06-01 22:04:51 +0700

    Add Common Services, Core Services, Register Dependencies

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SF.Application.Contracts.Customers;
+using SF.Application.Contracts.Customers.Dto;
+using SF.Domain.Shared.Base;
 
 namespace SmartFinance.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CustomerController : ControllerBase
+    public class CustomerController : BaseController<CustomerController>
     {
+        private readonly ICustomerService _customerService;
 
+        public CustomerController(ICustomerService customerService,
+                                 CommonInjectableService<CustomerController> commonInjectableService) : base(commonInjectableService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<List<CustomerDto>> GetAll()
+        {
+            try
+            {
+                return await _customerService.GetAllCustomers();
+            }
+            catch (Exception ex)
+            {
+                {
+                    Logger.LogError("GetAll::ERROR", ex);
+                    return null;
+                }
+            }
+        }
     }
 }
