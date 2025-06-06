ece29196ec31f8533be696e582f897e823305a20
namvph <hoainam11134@gmail.com>
2025-06-06 23:23:43 +0700

    Fix project files generated incorrertly

---
+++ SF.Application.Contracts/Shared/DtoBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.Application.Contracts.Shared
+{
+    [Serializable]
+    public class DtoBase
+    {
+        public DateTime Updated { get; set; }
+        public DtoBase() 
+        {
+            Updated = DateTime.Now;
+        }
+    }
+}
