617f2c600d8cde5a38666c29725b21bbb0bb2784
Nam <hoainam11134@gmail.com>
2026-01-28 17:08:29 +0700

    Configure setting files for working in company, add some new domain classes

--- SmartFinance.Domain/Customer/CustomerAggregate/Customer.cs
+++ SmartFinance.Domain/Customer/CustomerAggregate/Customer.cs
@@ -8,23 +8,15 @@ using System.Threading.Tasks;
 
 namespace SmartFinance.Domain.Customer.CustomerAggregate
 {
-    public class Customer: EntityBase<Guid>, IAggregateRoot
+    public abstract class Customer: EntityBase<Guid>, IAggregateRoot
     {
         public string CustomerNumber { get; set; }
 
         public string CustomerName { get; set; }
         public string PhoneNumber { get; set; }
         public string EmailAddress { get; set; }
+        public string BrokerId { get; set; }
+        public bool IsActive { get; set; }
 
-        private Customer() : base(Guid.NewGuid()){}
-
-        public Customer(string customerNumber, string customerName, string phoneNumber, string emailAddress) 
-            : base(Guid.NewGuid())
-        {
-            CustomerNumber = customerNumber;
-            CustomerName = customerName;
-            PhoneNumber = phoneNumber;
-            EmailAddress = emailAddress;
-        }
     }
 }
